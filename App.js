00390ef73322a0b02275dbe49c10723bf92cbf24
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-04-13 10:58:16 +0100

    34min

--- App.js
+++ App.js
@@ -3,17 +3,44 @@ import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.containerUp}>
         <Text>Current Weather</Text>
+        <Text>6</Text>
+      </View>
+      <View style={styles.containerMiddle}>
+        <Text>Some other header</Text>
+      </View>
+      <View style={styles.containerDown}>
+        <Text>Some other header</Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
+    backgroundColor: "pink",
+    borderColor: "red",
+    borderWidth: 10,
+    flex: 1,
+    flexDirection: "column",
+    padding: 20,
+  },
+  containerUp: {
     backgroundColor: "green",
+    flex: 1,
+    alignItems: "center",
+  },
+  containerMiddle: {
+    backgroundColor: "orange",
+    flex: 1,
+    alignItems: "flex-end",
+  },
+  containerDown: {
+    backgroundColor: "red",
+    flex: 2,
+    justifyContent: "center",
   },
 });
 
---
+++ README.md
@@ -0,0 +1,3 @@
+FreeCodeCamp tutorial about React Native: https://youtu.be/obH0Po_RdWk
+
+Weather App
